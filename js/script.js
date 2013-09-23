b86e6f8b3ed8b50210e4a4157122c3bc766ca20e
liptonit <lambertusben@hotmail.com>
2013-09-23 16:06:11 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+window.onload = function()
+{
+    var inputs = document.getElementsByTagName('input');
+//    console.log(inputs);
+    for (var i = 0; i < inputs.length; i++) {
+        console.log('test');
+        var input = inputs[i];
+//        console.log(input);
+        var value = input.value;
+        input.addEventListener("change", function() {
+            console.log(value);
+            if (!validate(value))
+            {
+                console.log("YES");
+                input.style.color = "red";
+            }
+        }
+        );
+    }
+
+    function validate(input) {
+        console.log('test', input);
+        input = input.replace(" ", "");
+        var regExpr = new RegExp("^\d*$");
+        if (!regExpr.test(input)) {
+            return false;
+        }
+        return true;
+    }
+};
