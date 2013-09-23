7d47d3b04d83cce3f565f2b62f116aaf5078f827
Henri Nijborg <henrinijborg@hotmail.com>
2013-09-23 16:39:25 +0200

    Fixed only number on input check

--- js/script.js
+++ js/script.js
@@ -6,30 +6,32 @@
 window.onload = function()
 {
     var inputs = document.getElementsByTagName('input');
-   console.log("INPUTS: ", inputs);
+    // console.log("INPUTS: ", inputs);
     for (var i = 0; i < inputs.length; i++) {
         var input = inputs[i];
-        console.log("INPUT: ", input);
-//        console.log(input);
+        // console.log("INPUT: ", input);
         var value = input.value;
         input.addEventListener("change", function() {
-            console.log(value);
-            if (!validate(value))
-            {
-                console.log("YES");
-                input.style.color = "red";
+            if (!validate(this.value)) {
+                this.style.border = "1px solid red";
             }
-        }
-        );
+        });
     }
 
-    function validate(input) {
-        console.log('test', input);
-        input = input.replace(" ", "");
-        var regExpr = new RegExp("^\d*$");
-        if (!regExpr.test(input)) {
-            return false;
+    function validate(value) {
+        // console.log('VALIDATE: ', value);
+        value = value.replace(" ", "");
+
+        if(value.length > 0) {
+            // console.log("VALUE FOUND: ", value);
+            var regExpr = new RegExp("^[0-9]*$");
+            if (regExpr.test(value)) {
+                // console.log("TESTED AND PASSED: ", value);
+                return true;
+            }
+            // console.log("TESTED AND FAILED: ", value);
         }
-        return true;
+        
+        return false;
     }
 };
