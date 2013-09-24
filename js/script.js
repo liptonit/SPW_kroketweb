fcfda398a692a132cdc66c8811b463e47d633286
Henri Nijborg <henrinijborg@hotmail.com>
2013-09-24 12:36:19 +0200

    Added total total row and fixed some stuff

--- js/script.js
+++ js/script.js
@@ -5,7 +5,30 @@
 
 window.onload = function()
 {
-    var inputs = document.getElementsByTagName('input');
+    var inputs = document.getElementById('itemlist').getElementsByTagName('input');
+    var totalTotalPrice = 0;
+
+    var  totalpriceF = function(splittedPrice) {
+                    var newPrice = "";
+                    if(splittedPrice.length == 1) {
+                        newPrice = splittedPrice[0]+",00";
+                    } else {
+                        if(splittedPrice[1].length == 1) {
+                            newPrice = splittedPrice[0]+","+splittedPrice[1]+"0";
+                        } else {
+                            newPrice = splittedPrice[0]+","+splittedPrice[1];
+                        }
+                    }
+
+                    var newPriceParts = newPrice.split(",");
+                    if(newPriceParts[1].length > 2) {
+                        newPrice = newPriceParts[0]+","+new String(newPriceParts[1].substring(0, 2));
+                    }
+
+                    return newPrice;
+                };
+
+
     // console.log("INPUTS: ", inputs);
     for (var i = 0; i < inputs.length; i++) {
         var input = inputs[i];
@@ -15,6 +38,7 @@ window.onload = function()
             if (!validate(this.value)) {
                 this.style.border = "1px solid red";
             } else {
+                this.removeEventListener();
                 var parent = this.parentNode.parentNode;
 
                 var childs = parent.children;
@@ -36,28 +60,43 @@ window.onload = function()
                 var pricePerPiece = parseFloat(price.innerHTML.replace(",", "."));
                 var totalprice = pricePerPiece * totalAmount;
 
+
+
                 var totalpricepieces = new String(totalprice).split(".");
 
-                totalprice = function() {
-                    if(totalpricepieces.length == 1) {
-                        return totalpricepieces[0]+",00";
-                    } else {
-                        if(totalpricepieces[1].length == 1) {
-                            return totalpricepieces[0]+","+totalpricepieces[1]+"0";
-                        } else {
-                            return totalpricepieces[0]+","+totalpricepieces[1];
-                        }
-                    }
-                };
+               
 
-                totalprice = totalprice();
-
+                totalprice = totalpriceF(totalpricepieces);
                 total.innerHTML = totalprice;
                 parent.appendChild(total);
 
                 var shoppingcar = document.getElementById('shoppingcar').tBodies.item(0).appendChild(parent);
+
+                totalTotalPrice = parseFloat(totalTotalPrice) + parseFloat(totalprice);
+                totalTotalPricePieces = new String(totalTotalPrice).split(",");
+                totalTotalPrice = totalpriceF(totalTotalPricePieces);
+                document.getElementById('totaltotal').innerHTML = totalTotalPrice;
+
+                addChangeEventToShopCarItem(this);
+                // this.addEventListener("change", function() {
+                //     this.parent.parent.childs[3].innerHTML = "";
+                //     var totalPrice = parseInt(this.value) * parseFloat(this.parent.parent.childs[2].innerHTML);
+                //     var totalPricePieces = new String(totalPrice).split(".");
+                //     totalPrice = totalpriceF(totalPricePieces);
+                //     this.parent.parent.childs[3].innerHTML = totalPrice;
+                // }, false);
             }
-        });
+        }, false);
+    }
+
+    function addChangeEventToShopCarItem(item) {
+         item.addEventListener("change", function() {
+                    item.parent.parent.childs[3].innerHTML = "";
+                    var totalPrice = parseInt(item.value) * parseFloat(item.parent.parent.childs[2].innerHTML);
+                    var totalPricePieces = new String(totalPrice).split(".");
+                    totalPrice = totalpriceF(totalPricePieces);
+                    item.parent.parent.childs[3].innerHTML = totalPrice;
+                }, false);
     }
 
     function validate(value) {
